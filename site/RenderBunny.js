e4d127469a51b640b6c7cef575e183e670843776
Run Yu <runyu@vt.edu>
2021-01-25 22:56:56 -0500

    basic mesh viewing with bunny obj

--- site/RenderBunny.js
+++ site/RenderBunny.js
@@ -20,20 +20,60 @@ export function MeshViewer(canvasDOM) {
   const vs = `#version 300 es
   layout (location=0) in vec3 vPos;
   layout (location=1) in vec3 vNormal;
+  uniform mat4 objMat;
   uniform mat4 viewMat;
   uniform mat4 perspectiveMat;
+  out vec3 normal;
 
   void main() {
-      gl_Position = perspectiveMat * viewMat * vec4(vPos, 1.0f);
+      gl_Position = perspectiveMat * viewMat * objMat * vec4(vPos, 1.0f);
+      normal = vNormal;
   }
   `;
 
   const fs = `#version 300 es
   precision highp float;
+  in vec3 normal;
   out vec4 finalColor;
 
+  struct DirLight
+  {
+    vec3 _dir;
+    vec3 _ambient;
+    vec3 _diffuse;
+    vec3 _specular;
+  };
+  uniform DirLight g_dirLight;
+
+  highp vec3 GetDirLight (DirLight light, vec3 normal, vec3 viewDir) 
+  {
+    //diffuse first
+    vec3 lightDir = normalize(-light._dir);
+    float diffuse = clamp (dot(lightDir, normal), 0.0f, 1.0f);
+    vec3 diffuseResult = diffuse * light._diffuse;
+
+        /*
+    //specular next
+    highp vec3 reflectDir = reflect (-lightDir, normal);
+    highp float specular = clamp (dot(reflectDir, viewDir), 0.0f, 1.0f);
+    specular = pow (specular, mat._shininess); 
+    highp vec3 specularResult = specular * light._specular * simTextColor;
+        //vec3(texture (mat._specularSampler2D, textureCoord));
+
+    //ambient last
+    highp vec3 ambientResult = light._ambient * simTextColor; 
+        //vec3(texture (mat._diffuseSampler2D, textureCoord));
+
+    //return (diffuseResult + specularResult + ambientResult);
+    return diffuseResult + ambientResult;
+    */
+    return diffuseResult;
+  }
+
+
   void main(){
-    finalColor = vec4(1.0, 0.0, 0.0, 1.0);
+    //finalColor = vec4(g_dirLight._specular, 1.0);
+    finalColor = vec4(GetDirLight(g_dirLight, normal, vec3(0.0, 0.0, 0.0)), 1.0);
   }
   `;
 
@@ -136,6 +176,9 @@ export function MeshViewer(canvasDOM) {
 
   function init() {
     const gl = this.gl;
+    gl.enable(gl.CULL_FACE);
+    gl.enable(gl.DEPTH_TEST);
+    gl.cullFace(gl.BACK);
     this.plainShader = new Shader(gl, vs, fs);
     this.meshData = parseObj(bunnyMeshDataObj);
     this.processedMesh = processMesh(this.meshData);
@@ -159,17 +202,27 @@ export function MeshViewer(canvasDOM) {
     gl.bindVertexArray(null);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
+    const objMat = mat4.create();
+    mat4.fromRotation(objMat, 0, vec3.fromValues(0.0, 1.0, 1.0));
+    this.rotatedYAngle = 0.0;
+
     const viewMat = mat4.create();
     mat4.lookAt(
       viewMat,
-      vec3.fromValues(0, 0.3, 0.2),
-      vec3.fromValues(0, 0, 0),
+      vec3.fromValues(0, 0.2, 0.15),
+      vec3.fromValues(0, 0.1, 0),
       vec3.fromValues(0, 1, 0)
     );
     //console.log("viewMat is: ", viewMat);
     const perspectiveMat = mat4.create();
-    mat4.perspective(perspectiveMat, degreeToRadian(90), 1, 0.1, 500);
+    mat4.perspective(perspectiveMat, degreeToRadian(90), 1, 0.01, 50);
 
+    gl.useProgram(this.plainShader.shaderProgram);
+    gl.uniformMatrix4fv(
+      gl.getUniformLocation(this.plainShader.shaderProgram, "objMat"),
+      false,
+      objMat
+    );
     gl.useProgram(this.plainShader.shaderProgram);
     gl.uniformMatrix4fv(
       gl.getUniformLocation(this.plainShader.shaderProgram, "viewMat"),
@@ -185,11 +238,69 @@ export function MeshViewer(canvasDOM) {
 
   function render(now) {
     const gl = this.gl;
-    gl.bindVertexArray(this.vao);
+    console.log("render");
     gl.useProgram(this.plainShader.shaderProgram);
+    const dirLight = {
+      dir: vec3.fromValues(0.0, -1.0, -0.5),
+      ambient: vec3.fromValues(0.1, 0.1, 0.1),
+      diffuse: vec3.fromValues(0.8, 0.9, 0.5),
+      specular: vec3.fromValues(1.0, 1.0, 1.0),
+    };
+    gl.uniform3f(
+      gl.getUniformLocation(this.plainShader.shaderProgram, "g_dirLight._dir"),
+      dirLight.dir[0],
+      dirLight.dir[1],
+      dirLight.dir[2]
+    );
+    gl.uniform3f(
+      gl.getUniformLocation(
+        this.plainShader.shaderProgram,
+        "g_dirLight._ambient"
+      ),
+      dirLight.ambient[0],
+      dirLight.ambient[1],
+      dirLight.ambient[2]
+    );
+    gl.uniform3f(
+      gl.getUniformLocation(
+        this.plainShader.shaderProgram,
+        "g_dirLight._diffuse"
+      ),
+      dirLight.diffuse[0],
+      dirLight.diffuse[1],
+      dirLight.diffuse[2]
+    );
+    gl.uniform3f(
+      gl.getUniformLocation(
+        this.plainShader.shaderProgram,
+        "g_dirLight._specular"
+      ),
+      dirLight.specular[0],
+      dirLight.specular[1],
+      dirLight.specular[2]
+    );
+
+    //compose the rotation based on mouse input
+    console.log("this.rotatedYAngle is: ", this.rotatedYAngle);
+    const objMat = mat4.create();
+    mat4.fromRotation(
+      objMat,
+      this.rotatedYAngle,
+      vec3.fromValues(0.0, 1.0, 0.0)
+    );
+    gl.uniformMatrix4fv(
+      gl.getUniformLocation(this.plainShader.shaderProgram, "objMat"),
+      false,
+      objMat
+    );
+
+    gl.bindVertexArray(this.vao);
+
     gl.clear(gl.COLOR_BUFFER_BIT, gl.DEPTH_BUFFER_BIT);
     gl.drawArrays(gl.TRIANGLES, 0, this.processedMesh.length / 6);
     gl.bindVertexArray(null);
+
+    this.curRequestedFrame = requestAnimationFrame(this.render);
   }
 
   function startRendering() {
